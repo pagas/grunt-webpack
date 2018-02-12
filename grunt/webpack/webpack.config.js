57f2ea2404d7b7d4ff12012153ffa24b0ac39651
Paulius <paulius@2simple.com>
2018-02-12 10:25:36 +0000

    commit webpack config file

---
+++ grunt/webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const rootPath = path.resolve(__dirname, '../..');
+const nodeModulesPath = path.resolve(__dirname, '../node_modules');
+
+console.log('npm path', nodeModulesPath);
+
+module.exports = {
+    entry: path.resolve(rootPath, './src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(rootPath, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js$/,
+                loaders: 'buble-loader',
+                include: path.resolve(rootPath, 'src'),
+                query: {
+                    objectAssign: 'Object.assign'
+                }
+            }
+        ]
+    },
+    resolve: {
+        modules: [nodeModulesPath],
+    },
+    devtool: 'inline-cheap-source-map',
+    watch: true,
+    devServer: {
+        contentBase: path.resolve(rootPath, "dist"),
+        compress: true,
+        port: 9000
+    }
+};
